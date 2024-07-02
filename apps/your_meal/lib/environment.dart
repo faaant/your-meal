f5bd3c656f3336659b42a86287174780d504fb55
faaant <71407013+faaant@users.noreply.github.com>
2024-07-02 21:05:32 +0300

    update

--- apps/your_meal/lib/environment.dart
+++ apps/your_meal/lib/environment.dart
@@ -1,3 +1,2 @@
-const apiUrl = 'https://stirring-alien-82.hasura.app/v1/graphql';
-const hasuraAdminSecret =
-    'FzG7EVgLcSjhWaOMZVULzgEc2bqU5eUynphCFWcbuAWX42ZrHK3IUe2mRcmUmbKJ';
+const apiUrl = 'your api url';
+const hasuraAdminSecret = 'your secret';
